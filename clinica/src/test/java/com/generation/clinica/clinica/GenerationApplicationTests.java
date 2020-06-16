6cfa4f6963a12704b19202a53ca255eb2a8263d8
Joao-lbraga <58825239+Joao-lbraga@users.noreply.github.com>
2020-06-16 02:32:38 -0300

    Aplicação spring de clinica medica

---
+++ clinica/src/test/java/com/generation/clinica/clinica/GenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.clinica.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
